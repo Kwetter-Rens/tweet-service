0c2fb0e86c2ba5632151176e1c65fcfec6fcbfb5
Rens Kloosterman <rens.kloosterman@student.fontys.nl>
2022-11-07 11:51:34 +0100

    Cors

--- src/main/java/com/kwetter/tweetservice/controllers/TweetController.java
+++ src/main/java/com/kwetter/tweetservice/controllers/TweetController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/tweet")
 public class TweetController {
     private final TweetService tweetService;
