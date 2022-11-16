50db96e0e18d8473c077b360da193d8fc072fe9e
rens-kloosterman <rens.kloosterman@student.fontys.nl>
2022-11-16 10:23:06 +0100

    d

--- src/main/java/com/kwetter/tweetservice/controllers/TweetController.java
+++ src/main/java/com/kwetter/tweetservice/controllers/TweetController.java
@@ -5,18 +5,23 @@ import com.kwetter.tweetservice.services.TweetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/tweet")
 public class TweetController {
+    @Autowired
     private final TweetService tweetService;
 
     public TweetController(TweetService tweetService) {
         this.tweetService = tweetService;
     }
+
 
     @PostMapping("/post")
     public ResponseEntity<Tweet> postTweet(@RequestBody Tweet tweet) {
