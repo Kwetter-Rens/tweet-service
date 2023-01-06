18ec55ccb5151a3877ad4df21b09d2121e9348da
Rens Kloosterman <rens.kloosterman@student.fontys.nl>
2023-01-06 23:14:42 +0100

    Update docker-build.sh

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -4,4 +4,5 @@ echo "$tag"
 #inloggen docker io repo
 docker login -u $username -p $password
 docker build . -t "$username/tweet-repo:$tag"
+docker images
 docker push "$username/tweet-service:$tag"
