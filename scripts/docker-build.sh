af7a9f44c14e4bcd84de825ced298f23da4a464b
Rens Kloosterman <rens.kloosterman@student.fontys.nl>
2023-01-06 23:06:56 +0100

    Update docker-build.sh

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,7 +2,7 @@ tag="${GITHUB_SHA:0:8}"
 echo "$tag"
 
 #inloggen docker io repo
-docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-docker build . -t $DOCKER_USER/tweet-repo:$tag -t $DOCKER_USER/tweet-repo:latest
-docker push $DOCKER_USER/tweet-service:$tag
-docker push $DOCKER_USER/tweet-service:latest
+docker login -u $username -p $password
+docker build . -t $username/tweet-repo:$tag -t $username/tweet-repo:latest
+docker push $username/tweet-service:$tag
+docker push $username/tweet-service:latest
